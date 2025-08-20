c92addd4edac6518d4ff95fb763ebd8c5603702a
Deepak Rathour <deepakrathour05tech@gmail.com>
2025-08-20 14:59:54 +0530

    Update again vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  // 👇 GitHub Pages ke liye repo ka naam
-  base: "/namandigital_task1_portfolio/",
+  base: process.env.NODE_ENV === 'production' ? '/namandigital_task1_portfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
